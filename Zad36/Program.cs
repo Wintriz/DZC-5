0f3f81a14da41b29ea915f39d732b555237eeae2
Andrey <rf5800@mail.ru>
2022-12-28 00:10:11 +0300

    hz

--- Zad36/Program.cs
+++ Zad36/Program.cs
@@ -3,3 +3,24 @@
 // [3, 7, 23, 12] -> 19
 
 // [-4, -6, 89, 6] -> 0
+
+int[] array = new int[4];
+// int odd = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 11);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 1)
+    {
+        // odd = odd + array[i];
+        Console.Write(array[i] + " ");
+    }
+
+}
+
+// Console.Write("Сумма элементов на нечетных позициях: ");
+
